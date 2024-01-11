77a2b694d6f386c174023af38349782939110d73
vjesus <vinicius.jesus@businessintegrator.com.br>
2024-01-11 13:06:36 -0300

    Adicionar arquivos de projeto.

---
+++ PROJETO_MVC_SITE_VENDAS/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROJETO_MVC_SITE_VENDAS.Models;
+using System.Diagnostics;
+
+namespace PROJETO_MVC_SITE_VENDAS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PROJETO_MVC_SITE_VENDAS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROJETO_MVC_SITE_VENDAS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
