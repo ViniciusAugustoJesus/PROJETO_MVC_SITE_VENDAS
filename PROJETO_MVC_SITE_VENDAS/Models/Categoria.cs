b3162aab762524e59c837cb8a8c35725d5cda737
vjesus <vinicius.jesus@businessintegrator.com.br>
2024-01-11 13:56:47 -0300

    Adição de novas classes

---
+++ PROJETO_MVC_SITE_VENDAS/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace PROJETO_MVC_SITE_VENDAS.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; } 
+        public List<Lanche> Lanches { get; set;}
+
+    }
+}
