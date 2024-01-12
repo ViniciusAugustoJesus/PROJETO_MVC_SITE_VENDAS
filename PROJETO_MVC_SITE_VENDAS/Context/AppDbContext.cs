ee62f938dc3e21e26fa325e92ec9b357aab91a19
vjesus <vinicius.jesus@businessintegrator.com.br>
2024-01-12 09:56:37 -0300

    Configuração da string connection

---
+++ PROJETO_MVC_SITE_VENDAS/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROJETO_MVC_SITE_VENDAS.Models;
+
+namespace PROJETO_MVC_SITE_VENDAS.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; } 
+        public DbSet<Lanche> Lanches { get; set; } 
+
+
+
+    }
+}
